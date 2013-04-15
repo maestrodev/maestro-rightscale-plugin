1d5f14553f02012599e1c8d2099619eb8d99f63f
Carlos Sanchez <csanchez@maestrodev.com>
2013-04-15 16:25:23 +0200

    Set errors without raising exceptions, and don't catch exceptions that are not handled

--- src/right_scale_worker.rb
+++ src/right_scale_worker.rb
@@ -14,125 +14,124 @@ module MaestroDev
     end
 
     def start
-      begin
-        Maestro.log.info "Starting RightScale Worker"
-        validate_fields
+      Maestro.log.info "Starting RightScale Worker"
+      validate_fields
 
-        server_name = get_field('nickname')
-        server_id = get_field('server_id')
+      server_name = get_field('nickname')
+      server_id = get_field('server_id')
 
-        raise 'Invalid fields, must provide nickname or server_id' if server_name.nil? and server_id.nil?
-
-        init_server_connection()
-        server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
-        if server.nil?
-          raise "No server matches #{server_name}"
-        end
-        Maestro.log.info "Found server, '#{server.nickname}'."
-
-        server.reload_current
-        server.start
-
-        write_output "Requested server to start #{server.rs_id}\n"
-        set_field('rightscale_server_id', server.rs_id)
-
-        server.wait_for_operational_with_dns
-
-        set_field('rightscale_ip_address', server.ip_address)
-        set_field('rightscale_private_ip_address', server.private_ip_address)
-
-        write_output "Server up at #{server.ip_address}\n"
-      rescue Exception => e
-        trace = e.backtrace.join("\n")
-        Maestro.log.error("#{e.message}\n#{trace}")
-        set_error e.message
+      if server_name.nil? and server_id.nil?
+        set_error('Invalid fields, must provide nickname or server_id')
+        return
       end
+
+      init_server_connection()
+      server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
+      if server.nil?
+        set_error "No server matches #{server_name}"
+        return
+      end
+      Maestro.log.info "Found server, '#{server.nickname}'."
+
+      server.reload_current
+      server.start
+
+      write_output "Requested server to start #{server.rs_id}\n"
+      set_field('rightscale_server_id', server.rs_id)
+
+      server.wait_for_operational_with_dns
+
+      set_field('rightscale_ip_address', server.ip_address)
+      set_field('rightscale_private_ip_address', server.private_ip_address)
+
+      write_output "Server up at #{server.ip_address}\n"
 
       Maestro.log.info "***********************Completed RightScale.start***************************"
     end
 
     def stop
-      begin
-        Maestro.log.info "Starting RightScale Worker"
-        validate_fields
+      Maestro.log.info "Starting RightScale Worker"
+      validate_fields
 
-        server_name = get_field('nickname')
-        server_id = get_field('server_id') || get_field('rightscale_server_id')
+      server_name = get_field('nickname')
+      server_id = get_field('server_id') || get_field('rightscale_server_id')
 
-        raise 'Invalid fields, must provide nickname or server_id' if server_name.nil? and server_id.nil?
-
-        init_server_connection()
-        if server_id
-          server = Server.find(:first) { |s| s.rs_id = server_id }
-          if server.nil?
-            raise "No server with id #{server_id}"
-          end
-          Maestro.log.info "Found server, '#{server.rs_id}'."
-        else
-          server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
-          if server.nil?
-            raise "No server matches #{server_name}"
-          end
-          Maestro.log.info "Found server, '#{server.nickname}'."
-        end
-
-        server.reload_current
-        server.stop
-
-        write_output "Requested server to stop #{server.rs_id}\n"
-
-        server.wait_for_state("stopped") if get_field('wait_until_stopped')
-
-        write_output "Server stopped\n"
-      rescue Exception => e
-        trace = e.backtrace.join("\n")
-        Maestro.log.error("#{e.message}\n#{trace}")
-        set_error e.message
+      if server_name.nil? and server_id.nil?
+        set_error('Invalid fields, must provide nickname or server_id')
+        return
       end
+
+      init_server_connection()
+      if server_id
+        server = Server.find(:first) { |s| s.rs_id = server_id }
+        if server.nil?
+          set_error "No server with id #{server_id}"
+          return
+        end
+        Maestro.log.info "Found server, '#{server.rs_id}'."
+      else
+        server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
+        if server.nil?
+          set_error "No server matches #{server_name}"
+          return
+        end
+        Maestro.log.info "Found server, '#{server.nickname}'."
+      end
+
+      server.reload_current
+      server.stop
+
+      write_output "Requested server to stop #{server.rs_id}\n"
+
+      server.wait_for_state("stopped") if get_field('wait_until_stopped')
+
+      write_output "Server stopped\n"
 
       Maestro.log.info "***********************Completed RightScale.stop***************************"
     end
 
     def wait
-      begin
-        Maestro.log.info "Starting RightScale Worker"
-        validate_fields
+      Maestro.log.info "Starting RightScale Worker"
+      validate_fields
 
-        server_name = get_field('nickname')
-        server_id = get_field('server_id') || get_field('rightscale_server_id')
+      server_name = get_field('nickname')
+      server_id = get_field('server_id') || get_field('rightscale_server_id')
 
-        state = get_field('state')
+      state = get_field('state')
 
-        raise 'Invalid fields, must provide nickname or server_id' if server_name.nil? and server_id.nil?
-        raise 'Invalid fields, must provide state' if state.nil?
-
-        init_server_connection()
-        if server_id
-          server = Server.find(:first) { |s| s.rs_id = server_id }
-          if server.nil?
-            raise "No server with id #{server_id}"
-          end
-          Maestro.log.info "Found server, '#{server.rs_id}'."
-        else
-          server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
-          if server.nil?
-            raise "No server matches #{server_name}"
-          end
-          Maestro.log.info "Found server, '#{server.nickname}'."
-        end
-
-        server.reload_current
-
-        write_output "Waiting until server #{server.rs_id} is #{state}\n"
-
-        server.wait_for_state(state)
-
-        write_output "Server is #{state}\n"
-      rescue Exception => e
-        trace = e.backtrace.join("\n")
-        Maestro.log.error("#{e.message}\n#{trace}")
-        set_error e.message
+      if server_name.nil? and server_id.nil?
+        set_error('Invalid fields, must provide nickname or server_id')
+        return
       end
+      if state.nil?
+        set_error 'Invalid fields, must provide state'
+        return
+      end
+
+      init_server_connection()
+      if server_id
+        server = Server.find(:first) { |s| s.rs_id = server_id }
+        if server.nil?
+          set_error "No server with id #{server_id}"
+          return
+        end
+        Maestro.log.info "Found server, '#{server.rs_id}'."
+      else
+        server = Server.find(:first) { |s| s.nickname =~ /#{server_name}/ }
+        if server.nil?
+          set_error "No server matches #{server_name}"
+          return
+        end
+        Maestro.log.info "Found server, '#{server.nickname}'."
+      end
+
+      server.reload_current
+
+      write_output "Waiting until server #{server.rs_id} is #{state}\n"
+
+      server.wait_for_state(state)
+
+      write_output "Server is #{state}\n"
 
       Maestro.log.info "***********************Completed RightScale.wait***************************"
     end
