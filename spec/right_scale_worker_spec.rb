f70ce982949a16f3626bab66b013fe41dcaacb49
Carlos Sanchez <csanchez@maestrodev.com>
2013-04-23 01:06:05 +0200

    Store all servers started in different tasks in a composition
    Use naming convention from fog plugin
    Add specs with mocha

--- spec/right_scale_worker_spec.rb
+++ spec/right_scale_worker_spec.rb
@@ -9,127 +9,132 @@ NICKNAME = 'RSB'
 
 describe MaestroDev::RightScaleWorker do
 
+  let (:participant) { described_class.new }
+
+  let (:default_fields) { {
+    'account_id' => '11111',
+    'username' => 'jdoe@acme.com',
+    'password' => 'mypasswd'}
+  }
+
   before :each do
-    @test_participant = MaestroDev::RightScaleWorker.new
-    @test_participant.stubs(:write_output)
+    participant.stubs(:write_output)
   end
 
-  it "should fail to start a server with wrong credentials" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'nickname' => NICKNAME,
-        'account_id' => '1234',
-        'username' => 'test@example.com',
-        'password' => 'qwerty',
-    }})
+  describe 'start' do
 
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
+    def start(fields)
+      wi = Ruote::Workitem.new({'fields' => default_fields.merge('nickname' => NICKNAME)})
+      participant.expects(:workitem => wi.to_h).at_least_once
+      participant.start
+    end
 
-    @test_participant.start
+    it "should fail to start a server with wrong credentials" do
+      start(default_fields.merge('nickname' => NICKNAME))
+      participant.error.should eql('Invalid credentials provided: 401 Unauthorized')
+    end
 
-    wi.fields['__error__'].should eql('Invalid credentials provided: 401 Unauthorized')
+    # enable to test
+    xit "should start a server with valid credentials" do
+      wi = Ruote::Workitem.new({'fields' => {
+          'nickname' => NICKNAME,
+          'account_id' => VALID_ACCOUNT_ID,
+          'username' => VALID_USERNAME,
+          'password' => VALID_PASSWORD,
+          'api_url' => 'https://us-3.rightscale.com'
+      }})
+
+      participant.expects(:workitem => wi.to_h).at_least_once
+      participant.start
+      participant.error.should be_nil
+      wi.fields['rightscale_ip_address'].should_not be_nil
+      wi.fields['rightscale_server_id'].should_not be_nil
+    end
+
+    it "should fail to validate if no nickname and no server_id" do
+      start(default_fields)
+      participant.error.should eql('Invalid fields, must provide nickname or server_id')
+    end
+
+    it "should fail to validate if no account_id" do
+      start(default_fields.reject{|k,v| k=='account_id'})
+      participant.error.should eql('Invalid fields, must provide nickname or server_id, account_id')
+    end
+
+    it "should fail to validate if no username" do
+      start(default_fields.reject{|k,v| k=='username'})
+      participant.error.should eql('Invalid fields, must provide nickname or server_id, username')
+    end
+
+    it "should fail to validate if no password" do
+      start(default_fields.reject{|k,v| k=='password'})
+      participant.error.should eql('Invalid fields, must provide nickname or server_id, password')
+    end
   end
 
-  # enable to test
-  xit "should start a server with valid credentials" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'nickname' => NICKNAME,
-        'account_id' => VALID_ACCOUNT_ID,
-        'username' => VALID_USERNAME,
-        'password' => VALID_PASSWORD,
-        'api_url' => 'https://us-3.rightscale.com'
-    }})
+  describe 'stop' do
 
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
+    let(:instance) { mock('instance') }
+    let(:server) { mock('server', :current_instance => instance, :name => 'myserver', :href => "/api/servers/1") }
+    let(:servers) { mock('servers') }
 
-    @test_participant.start
+    before(:each) do
+      participant.stubs(:init_server_connection)
+      participant.client = stub('client', :servers => servers)
+    end
 
-    wi.fields['__error__'].should be_nil
-    wi.fields['rightscale_ip_address'].should_not be_nil
-    wi.fields['rightscale_server_id'].should_not be_nil
+    def stop(fields)
+      wi = Ruote::Workitem.new({'fields' => default_fields.merge(fields)})
+      participant.expects(:workitem => wi.to_h).at_least_once
+      instance.expects(:terminate)
+      participant.stop
+      participant.error.should be_nil
+    end
+
+    it "should stop servers passed as field server_id" do
+      servers.expects(:index).with(:id => 1).returns(server)
+      stop({'server_id' => 1})
+    end
+
+    it "should stop servers passed as field nickname" do
+      servers.expects(:index).with(:filter => ["name==myserver"]).returns([server])
+      stop({'nickname' => 'myserver'})
+    end
+
+    it "should stop servers stored by start task" do
+      servers.expects(:index).with(:id => '1').returns(server)
+      servers.expects(:index).with(:id => '2').raises(RightApi::Exceptions::ApiException,
+        "Error: HTTP Code: 422, Response body: ResourceNotFound: Couldn't find Server with ID=2")
+      stop({'rightscale_ids' => ['1', '2']})
+    end
+
+    # enable to test
+    xit "should stop a server with valid credentials" do
+      wi = Ruote::Workitem.new({'fields' => {
+          'nickname' => NICKNAME,
+          'account_id' => VALID_ACCOUNT_ID,
+          'username' => VALID_USERNAME,
+          'password' => VALID_PASSWORD,
+      }})
+
+      participant.expects(:workitem => wi.to_h).at_least_once
+      participant.stop
+      participant.error.should be_nil
+    end
+
+    # enable to test
+    xit "should stop a server using existing field item" do
+      wi = Ruote::Workitem.new({'fields' => {
+          'rightscale_server_id' => '1234',
+          'account_id' => VALID_ACCOUNT_ID,
+          'username' => VALID_USERNAME,
+          'password' => VALID_PASSWORD,
+      }})
+
+      participant.expects(:workitem => wi.to_h).at_least_once
+      participant.stop
+      participant.error.should be_nil
+    end
   end
 
-  # enable to test
-  xit "should stop a server with valid credentials" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'nickname' => NICKNAME,
-        'account_id' => VALID_ACCOUNT_ID,
-        'username' => VALID_USERNAME,
-        'password' => VALID_PASSWORD,
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.stop
-
-    wi.fields['__error__'].should be_nil
-  end
-
-  # enable to test
-  xit "should stop a server using existing field item" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'rightscale_server_id' => '1234',
-        'account_id' => VALID_ACCOUNT_ID,
-        'username' => VALID_USERNAME,
-        'password' => VALID_PASSWORD,
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.stop
-
-    wi.fields['__error__'].should be_nil
-  end
-
-  it "should fail to validate if no nickname and no server_id" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'account_id' => '1234',
-        'username' => 'test@example.com',
-        'password' => 'qwerty',
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.start
-
-    wi.fields['__error__'].should eql('Invalid fields, must provide nickname or server_id')
-  end
-
-  it "should fail to validate if no account_id" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'username' => 'test@example.com',
-        'password' => 'qwerty',
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.start
-
-    wi.fields['__error__'].should eql('Invalid fields, must provide nickname or server_id, account_id')
-  end
-
-  it "should fail to validate if no username" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'account_id' => '1234',
-        'password' => 'qwerty',
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.start
-
-    wi.fields['__error__'].should eql('Invalid fields, must provide nickname or server_id, username')
-  end
-
-  it "should fail to validate if no password" do
-    wi = Ruote::Workitem.new({'fields' => {
-        'account_id' => '1234',
-        'username' => 'test@example.com',
-    }})
-
-    @test_participant.expects(:workitem => wi.to_h).at_least_once
-
-    @test_participant.start
-
-    wi.fields['__error__'].should eql('Invalid fields, must provide nickname or server_id, password')
-  end
 end
--- src/right_scale_worker.rb
+++ src/right_scale_worker.rb
@@ -5,9 +5,15 @@ require 'rubygems'
 module MaestroDev
   class RightScaleWorker < Maestro::MaestroWorker
 
+    attr_accessor :client
+
+    def provider
+      'rightscale'
+    end
+
     def validate_fields(server_id)
       missing_fields = []
-      if get_field('nickname').nil? and server_id.nil?
+      if get_field('nickname').nil? and (server_id.nil? or server_id.empty?)
         missing_fields << 'nickname or server_id'
       end
       for f in %w(account_id username password)
@@ -54,19 +60,30 @@ module MaestroDev
         end
       end
 
-      server_id = File.basename server.href
+      server_id = get_server_id(server)
 
       write_output "Requested server to start #{server_id}\n"
-      set_field('rightscale_server_id', server_id)
+      set_field('rightscale_server_id', server_id) # deprecated
+      set_field("#{provider}_ids", (get_field("#{provider}_ids") || []) << server_id)
+      set_field("cloud_ids", (get_field("cloud_ids") || []) << server_id)
 
       wait_for_state('operational', server_id)
       return if error?
 
       instance = instance_resource.show
       ip_address = instance.public_ip_addresses.first
-      set_field('rightscale_ip_address', ip_address)
       private_ip_address = instance.private_ip_addresses.first
-      set_field('rightscale_private_ip_address', private_ip_address)
+
+      # save some values in the workitem so they are accessible for deprovision and other tasks
+      # using same naming as the fog plugin
+      set_field('rightscale_ip_address', ip_address) # deprecated
+      set_field('rightscale_private_ip_address', private_ip_address) # deprecated
+
+      set_field("#{provider}_private_ips", (get_field("#{provider}_private_ips") || []) << private_ip_address)
+      set_field("cloud_private_ips", (get_field("cloud_private_ips") || []) << private_ip_address)
+      set_field("#{provider}_ips", (get_field("#{provider}_ips") || []) << ip_address)
+      set_field("cloud_ips", (get_field("cloud_ips") || []) << ip_address)
+
 
       context_servers = read_output_value('rightscale_servers') || {}
       context_servers[server_id] = {
@@ -87,50 +104,73 @@ module MaestroDev
     end
 
     def stop
-      Maestro.log.info "Starting RightScale Worker"
+      Maestro.log.info "Stopping RightScale servers"
 
       server_name = get_field('nickname')
-      server_id = get_field('server_id') || get_field('rightscale_server_id')
 
-      validate_fields(server_id)
+      # what to stop
+      server_id = get_field('server_id')
+      if server_id and server_id > 0
+        # stop server id set in task
+        server_ids = [server_id]
+      elsif server_name
+        # stop server name set in task
+        server_ids = nil
+      else
+        # stop previously started servers
+        server_ids = get_field("#{provider}_ids") || []
+      end
+
+      validate_fields(server_ids)
       return if error?
 
       init_server_connection()
-      if server_id and server_id > 0
-        server = @client.servers(:id => server_id).show
-        if server.nil?
-          set_error "No server with id #{server_id}"
-          return
-        end
-        Maestro.log.info "Found server, '#{server_id}'."
+
+      # stop servers
+      servers = []
+      if server_ids
+        servers = server_ids.map do |id|
+          begin
+            @client.servers.index(:id => id)
+          rescue RightApi::Exceptions::ApiException => e
+            write_output "Unable to get server with id #{id}: #{e.message}. Ignoring\n"
+            nil
+          end
+        end.compact
       else
-        server = @client.servers.index(:filter => ["name==#{server_name}"]).first
-        if server.nil?
-          set_error "No server matches #{server_name}"
-          return
+        servers = @client.servers.index(:filter => ["name==#{server_name}"])
+      end
+      servers.each {|s| stop_server(s)}
+
+      if get_field('wait_until_stopped')
+        servers.each do |server|
+          server_id = get_server_id(server)
+          msg = "Waiting for server to stop [#{server_id}] #{server.name}"
+          write_output "#{msg}\n"
+          Maestro.log.info msg
+          wait_for_state("inactive", server_id)
         end
-        Maestro.log.info "Found server, '#{server.name}'."
       end
 
-      server_id = File.basename server.href
-
-      server.current_instance.terminate
-
-      write_output "Requested server to stop #{server_id}\n"
-
-      wait_for_state("inactive", server_id) if get_field('wait_until_stopped')
-      return if error?
-
       context_servers = read_output_value('rightscale_servers') || {}
-      context_servers[server_id][:state] = @client.servers(:id => server_id).show.state if context_servers[server_id]
+      servers.each do |server|
+        context_servers[server.id][:state] = server.state if context_servers[server_id]
+      end
 
-      write_output "Server stopped\n"
+      write_output "Servers stopped\n"
 
       Maestro.log.info "***********************Completed RightScale.stop***************************"
     end
 
+    def stop_server(server)
+      msg = "Stopping server [#{get_server_id(server)}] #{server.name}"
+      write_output "#{msg}\n"
+      Maestro.log.info msg
+      server.current_instance.terminate
+    end
+
     def wait
-      Maestro.log.info "Starting RightScale Worker"
+      Maestro.log.info "Waiting for RightScale servers"
 
       # TODO: validate states
       state = get_field('state')
@@ -142,7 +182,7 @@ module MaestroDev
       return if error?
 
       init_server_connection()
-      if server_id and server_id > 0
+      if server_id and server_id.to_i > 0
         server = @client.servers(:id => server_id).show
         if server.nil?
           set_error "No server with id #{server_id}"
@@ -158,7 +198,7 @@ module MaestroDev
         Maestro.log.info "Found server, '#{server.name}'."
       end
 
-      server_id = File.basename server.href
+      server_id = get_server_id(server)
 
       write_output "Waiting until server #{server_id} is #{state}\n"
 
@@ -217,8 +257,15 @@ module MaestroDev
     end
 
     def close()
-
     end
+
+    private
+
+    # get the id of a server object returned by the API
+    def get_server_id(server)
+      File.basename server.href
+    end
+
   end
 
   class LogWrapper
