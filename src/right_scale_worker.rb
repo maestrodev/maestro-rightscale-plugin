7fad3d7e2efe2279562698665251375530f6ea5e
David Castro <dcastro@maestrodev.com>
2013-05-01 21:12:38 -0700

    MAESTRO-2816 start of merging in from master, namely the execute and get server tasks

--- src/right_scale_worker.rb
+++ src/right_scale_worker.rb
@@ -118,6 +118,77 @@ module MaestroDev
       return client
     end
 
+    # TODO: refactor to use helper, the trim down
+    def get_server
+      Maestro.log.info "Retrieving RightScale server information into the Composition"
+
+      # TODO: much duplication with start, but refactor after other changes for deployments land
+
+      server_name = get_field('nickname')
+      # TODO: should be used, or make nickname required?
+      server_id = get_field('server_id')
+
+      validate_fields(server_id)
+      return if error?
+
+      begin
+        init_server_connection()
+      rescue RestClient::Unauthorized => e
+        set_error "Invalid credentials provided: #{e.message}"
+        return
+      end
+
+      server = @client.servers.index(:filter => ["name==#{server_name}"]).first
+      if server.nil?
+        set_error "No server matches #{server_name}"
+        return
+      end
+
+      Maestro.log.info "Found server, '#{server.name}'."
+
+      server_id = get_server_id(server)
+
+      set_field('rightscale_server_id', server_id) # deprecated
+      set_field("#{provider}_ids", (get_field("#{provider}_ids") || []) << server_id)
+      set_field("cloud_ids", (get_field("cloud_ids") || []) << server_id)
+
+      context_server = {
+          :name => server.name,
+          :state => server.state,
+      }
+      if server.respond_to? :current_instance
+        instance = server.current_instance.show
+        ip_address = instance.public_ip_addresses.first
+        private_ip_address = instance.private_ip_addresses.first
+
+        # save some values in the workitem so they are accessible for deprovision and other tasks
+        # using same naming as the fog plugin
+        set_field('rightscale_ip_address', ip_address) # deprecated
+        set_field('rightscale_private_ip_address', private_ip_address) # deprecated
+
+        set_field("#{provider}_private_ips", (get_field("#{provider}_private_ips") || []) << private_ip_address)
+        set_field("cloud_private_ips", (get_field("cloud_private_ips") || []) << private_ip_address)
+        set_field("#{provider}_ips", (get_field("#{provider}_ips") || []) << ip_address)
+        set_field("cloud_ips", (get_field("cloud_ips") || []) << ip_address)
+        context_server = {
+            :public_ip_address => ip_address,
+            :private_ip_address => private_ip_address,
+            :multi_cloud_image => instance.multi_cloud_image.show.name,
+            :server_template => instance.server_template.show.name,
+            :deployment => instance.deployment.show.name,
+            :resource_uid => instance.resource_uid
+        }.merge context_server
+      end
+
+      write_output "Server information: #{PP.pp context_server, out = ''}\n"
+
+      context_servers = read_output_value('rightscale_servers') || {}
+      context_servers[server_id] = context_server
+      save_output_value('rightscale_servers', context_servers)
+
+      Maestro.log.info "***********************Completed RightScale.get_server***************************"
+    end
+
     def start
       Maestro.log.info "Starting RightScale Worker"
 
@@ -492,6 +563,110 @@ module MaestroDev
       }
 
       Maestro.log.info "***********************Completed RightScale.stop***************************"
+    end
+
+    # TODO: pull code into helper and trim this down
+    def execute
+      Maestro.log.info "Executing RightScript"
+
+      # TODO: much duplication with stop, but refactor after other changes for deployments land
+
+      server_name = get_field('nickname')
+
+      # what to execute on
+      server_id = get_field('server_id')
+      if server_id and server_id > 0
+        # stop server id set in task
+        server_ids = [server_id]
+      elsif server_name
+        # stop server name set in task
+        server_ids = nil
+      else
+        # stop previously started servers
+        server_ids = get_field("#{provider}_ids") || []
+      end
+
+      validate_fields(server_ids)
+      return if error?
+
+      recipe = get_field('recipe')
+      unless recipe
+        set_error("Invalid fields, must provide recipe")
+        return
+      end
+
+      init_server_connection()
+
+      # execute on servers
+      servers = []
+      if server_ids
+        servers = server_ids.map do |id|
+          begin
+            @client.servers.index(:id => id)
+          rescue RightApi::Exceptions::ApiException => e
+            write_output "Unable to get server with id #{id}: #{e.message}. Ignoring\n"
+            nil
+          end
+        end.compact
+      else
+        servers = @client.servers.index(:filter => ["name==#{server_name}"])
+      end
+      tasks = []
+      servers.each do |s|
+        begin
+          task = execute_on_server(recipe, s)
+          tasks << task
+        rescue RightApi::Exceptions::ApiException => e
+          msg = "Error executing on server [#{get_server_id(s)}] #{s.name}: #{e.message}. Ignoring"
+          Maestro.log.error msg
+          write_output "#{msg}\n"
+        end
+      end
+
+      if get_field('wait_for_completion')
+        tasks.each do |t|
+          wait_for_task(recipe, t)
+        end
+      end
+
+      Maestro.log.info "***********************Completed RightScale.execute***************************"
+    end
+
+    def execute_on_server(recipe, s)
+      msg = "Executing '#{recipe}' on server [#{get_server_id(s)}] #{s.name}"
+      write_output "#{msg}\n"
+      Maestro.log.info msg
+      s.current_instance.show.run_executable :recipe_name => recipe
+    end
+
+    # wait for task to complete
+    # Timeout after 600s without changing state, check every 5s
+    def wait_for_task(recipe, task, timeout=600, interval=5)
+      desired_summary = "completed: #{recipe}"
+      last_state = nil
+      i = 0
+      while i <= timeout do
+        summary = task.show.summary
+        return true if summary == desired_summary
+
+        # print in the output if task changed state, and reset timeout
+        if summary != last_state
+          last_state = summary
+          i = 0
+          msg = "Task is now #{summary}, waiting for #{desired_summary}"
+          write_output "#{msg}\n"
+          Maestro.log.info msg
+        else
+          Maestro.log.debug "Server state is #{summary}, waiting for #{desired_summary} (#{i}/#{timeout})"
+        end
+
+        sleep interval
+        i += interval
+      end
+
+      msg = "Timed out after #{timeout}s waiting for receipe #{recipe} to complete, is currently #{task.show.summary}"
+      Maestro.log.info msg
+      set_error msg
     end
 
     private
